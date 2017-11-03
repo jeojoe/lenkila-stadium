dbda318e0850f4fc434ab2325638143867ae56e2
Jirat Onaree <jirat.onaree@gmail.com>
2017-11-03 14:30:17 +0700

    Component : Layout

---
+++ components/Layout.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import Head from 'next/head';
+
+const Layout = ({ children }) => (
+  <div>
+    <Head>
+      <title>Lenkila : ระบบจัดการสนาม</title>
+    </Head>
+  </div>
+);
+
+export default Layout;
---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { Layout } from '../components';
+
+class Index extends Component {
+  render() {
+    return (
+      <Layout>sdkfsdjf</Layout>
+    );
+  }
+}
+
+export default Index;
