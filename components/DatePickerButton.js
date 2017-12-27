5b477417f467479fb931209610709ac29b99e7cc
Jirat Onaree <jirat.onaree@gmail.com>
2017-12-27 20:16:16 +0700

    Choose date logic in index page

--- components/DatePickerButton.js
+++ components/DatePickerButton.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import moment from 'moment';
 
 const DatePickerButton = ({ value, onClick }) => (
   <button
-    className="btn btn-light"
+    className="btn btn-info"
     onClick={onClick}
   >
-    {value}
+    {moment(value, 'DD/MM/YYYY').format('วันdddd ที่ DD MMMM YYYY')}
+    <i className="fa fa-pencil" />
+    <style jsx>{`
+      .fa {
+        margin-left: 10px;
+      }
+    `}
+    </style>
   </button>
 );
 
--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,43 @@
 // รายการวันนี้
 import React, { Component } from 'react';
-import { Layout } from '../components';
+import Link from 'next/link';
+import DatePicker from 'react-datepicker';
+import moment from 'moment';
+
+import { Layout, DatePickerButton } from '../components';
 
 class Index extends Component {
   state = {
-    h: '',
+    currentDate: moment(),
   }
+
   render() {
     return (
       <Layout title="รายการวันนี้">
-        <p>sdlkfjskdjf</p>
+        <div className="d-flex align-items-center">
+          <h5 style={{ margin: '0 10px' }}>รายการของ : </h5>
+          <DatePicker
+            customInput={<DatePickerButton />}
+            selected={this.state.currentDate}
+            onChange={currentDate => this.setState({ currentDate })}
+          />
+          <button
+            onClick={() => this.setState({ currentDate: moment() })}
+            className="btn btn-outline-info pick-td-btn"
+          >
+            เลือกวันนี้
+          </button>
+        </div>
+        <style>
+          {`
+            .top-btn {
+              margin-right: 20px;
+            }
+            .pick-td-btn {
+              margin-left: 10px;
+            }
+          `}
+        </style>
       </Layout>
     );
   }
