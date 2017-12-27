b683032489ffb858459ce3698e6c32bb4a0d0a92
Jirat Onaree <jirat.onaree@gmail.com>
2017-12-27 17:44:11 +0700

    Update route naming

---
+++ pages/field-management.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class FieldManagement extends Component {
+  render() {
+    return (
+      <div>ระบบจัดการสนาม</div>
+    );
+  }
+}
+
+export default FieldManagement;
